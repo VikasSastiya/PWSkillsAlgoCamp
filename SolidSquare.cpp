dad75248daef7495ddcb9d3ce834e80da27ab86e
VikasSastiya <vikassastiya06@gmail.com>
2024-07-28 21:16:36 +0530

    SolidSquare.cpp

--- SolidSquare.cpp
+++ SolidSquare.cpp
@@ -4,13 +4,10 @@ int main(){
     int  n;
     cout<<"No of rows : ";
     cin>>n;
-    int m;
-    cout<<"No of columns : ";
-    cin>>m;
     for(int i=1;i<=n;i++) {
-        for(int j=1;j<=m;j++) {
+        for(int i=1;i<=n;i++) {
     cout<<"* ";
         }
-         // cout<<endl;
+      cout<<endl;
 }
 }
