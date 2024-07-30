db5f4f12ebba08607ee41a125478cb1097179d09
VikasSastiya <vikassastiya06@gmail.com>
2024-07-30 12:46:52 +0530

    StarTriangleReverse.cpp

--- SolidSquare.cpp
+++ SolidSquare.cpp
@@ -5,7 +5,7 @@ int main(){
     cout<<"No of rows : ";
     cin>>n;
     for(int i=1;i<=n;i++) {
-        for(int j=1;j<=n;i++) {
+        for(int j=1;j<=n;j++) {
     cout<<"* ";
         }
       cout<<endl;
