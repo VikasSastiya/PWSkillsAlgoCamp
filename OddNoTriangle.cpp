b41cb0dd45791e36063e63474a33ea982a1b115c
VikasSastiya <vikassastiya06@gmail.com>
2024-07-31 12:51:17 +0530

    OddNoTriange.cppAlternative2

--- OddNoTriangle.cpp
+++ OddNoTriangle.cpp
@@ -5,8 +5,10 @@ int main() {
     cout<<"Enter no of rows";
     cin>>n;
     for(int i=1;i<=n;i++){
+        int a=1;
         for(int j=1;j<=2*i-1;j+=2){
-            cout<<j;
+            cout<<a;
+            a+=2;
         }
         cout<<endl;
     }
