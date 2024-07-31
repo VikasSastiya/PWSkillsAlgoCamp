a4413bf1c8c13440fc192c46112b6a9ed0030ae3
VikasSastiya <vikassastiya06@gmail.com>
2024-07-31 13:08:18 +0530

    AlphabetSquare.cpp

---
+++ AiphabetSquare.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"no of rows :";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        int a=65;
+        for(int j=1;j<=n;j++){
+          cout<<(char)(a)<<" ";
+          a+=1;
+        }
+        cout<<endl;
+    }
+}
