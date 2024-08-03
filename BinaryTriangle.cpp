6c4f95a624703c64641a3696a3d907010de3601e
VikasSastiya <vikassastiya06@gmail.com>
2024-08-03 12:34:24 +0530

    BinaryTriangleUsing Flipping.cpp

--- BinaryTriangle.cpp
+++ BinaryTriangle.cpp
@@ -4,14 +4,15 @@ int main(){
     int n;
     cout<<"no of rows :";
     cin>>n;
+      int a=5;
     for(int i=1;i<=n;i++){
+         if(i%2!=0) a=1; // row no odd
+         else a=0;   // row no even
        for(int j=1;j<=i;j++){
-        if((i+j) % 2 == 0) {
-            cout<<1;
-        }
-      else {
-        cout<<0;
-      }   
+        cout<<a;
+        //flipping
+        if(a==1) a=0;
+        else a=1;
    }
    cout<<endl;
     }
