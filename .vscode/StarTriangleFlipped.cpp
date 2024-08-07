dfef2719aada855452c9d60bf09ae29069c15adf
VikasSastiya <vikassastiya06@gmail.com>
2024-08-07 21:06:52 +0530

    another method

--- .vscode/StarTriangleFlipped.cpp
+++ .vscode/StarTriangleFlipped.cpp
@@ -5,14 +5,14 @@ int main(){
     cout<<"no of rows :";
     cin>>n;
 for(int i=1;i<=n;i++){
-          for(int j=1;j<=(n-i);j++){
-      cout<<" ";
-}
-    for(int k=1;k<=i;k++){
+    for(int k=1;k<=n;k++){
+       if((i+k)>=n+1){
       cout<<"*"; 
+       }
+       else {
+        cout<<" ";
+       }
     }
- 
-    
-   cout<<endl;
+       cout<<endl;
 }
 }
